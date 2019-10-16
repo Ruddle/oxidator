c90c80672affcfc7ce1723e1468563bcf4ce2e15
Thomas SIMON <thomas.simon.work@gmail.com>
2019-10-16 17:08:45 +0200

    use nphysics default ground

--- src/phy_state.rs
+++ src/phy_state.rs
@@ -12,7 +12,7 @@ use na::{Isometry3, Matrix3, Matrix4, Point3};
 use nalgebra::DMatrix;
 use ncollide3d::shape::{Ball, HeightField, ShapeHandle};
 use nphysics3d::math::{Inertia, Velocity};
-use nphysics3d::object::{BodyStatus, RigidBodyDesc};
+use nphysics3d::object::{BodyStatus, Ground, RigidBodyDesc};
 
 pub struct InnerState {
     mechanical_world: DefaultMechanicalWorld<f32>,
@@ -126,56 +126,61 @@ impl InnerState {
             .integration_parameters
             .allowed_angular_error = 0.01;
         let step = 2048;
+
+        let ground_handle = bodies.insert(Ground::new());
         for i in (0..2048 / step).step_by(1) {
             for j in (0..2048 / step).step_by(1) {
-                let ground_rb = RigidBodyDesc::new()
-                    .translation(Vector3::new(
-                        (i * step) as f32 + (step / 2) as f32,
-                        0.0,
-                        (j * step) as f32 + (step / 2) as f32,
-                    ))
-                    .status(BodyStatus::Static)
-                    .gravity_enabled(false)
-                    .build();
-
-                let ground_rb_handle = bodies.insert(ground_rb);
-
                 let heights = DMatrix::from_fn(step, step, |a, b| {
                     crate::heightmap::z((i * step) as f32 + b as f32, (j * step) as f32 + a as f32)
                 });
-                let mut heightfield: HeightField<f32> =
+                let heightfield: HeightField<f32> =
                     HeightField::new(heights, Vector3::new(step as f32, 1.0, step as f32));
 
-                let shape =
-                    ShapeHandle::new(ncollide3d::shape::Plane::new(Vector3::<f32>::y_axis()));
+                //                let shape =
+                //                    ShapeHandle::new(ncollide3d::shape::Plane::new(Vector3::<f32>::y_axis()));
+                //
+                //                let quad = ncollide3d::procedural::quad(2048.0, 2048.0, 1, 1);
+                //                let indices = quad
+                //                    .flat_indices()
+                //                    .chunks(3)
+                //                    .map(|is| Point3::new(is[0] as usize, is[2] as usize, is[1] as usize))
+                //                    .collect();
+                //
+                //                let mut vertices = quad.coords;
+                //
+                //                for p in &mut vertices {
+                //                    p.x = p.x + 1024.0;
+                //                    p.z = p.y + 1024.0;
+                //                    p.y = crate::heightmap::z(p.x, p.z);
+                //                }
+                //
+                //                let shape =
+                //                    ShapeHandle::new(ncollide3d::shape::TriMesh::new(vertices, indices, None));
 
                 let shape = ShapeHandle::new(heightfield);
                 //    let shape = ShapeHandle::new(Ball::new(1.0));
-                let ground_col =
-                    ColliderDesc::new(shape).build(BodyPartHandle(ground_rb_handle, 0));
+                let ground_col = ColliderDesc::new(shape)
+                    .position(Isometry3::translation(1024.0, 0.0, 1024.0))
+                    .build(BodyPartHandle(ground_handle, 0));
                 let ground_col_handle = colliders.insert(ground_col);
             }
         }
 
         let mut cubes = Vec::new();
-        for i in (100..200).step_by(4) {
-            for j in (100..200).step_by(4) {
+        for i in (100..2000).step_by(40) {
+            for j in (100..2000).step_by(40) {
                 let ball_rb = RigidBodyDesc::new()
                     .translation(Vector3::y() * 5.0)
                     .rotation(Vector3::y() * 5.0)
                     .position(Isometry3::new(
-                        Vector3::new(i as f32, 100.0, j as f32),
+                        Vector3::new(i as f32, 200.0, j as f32),
                         Vector3::y() * 3.141592,
                     ))
-                    //        .velocity(Velocity::linear(1.0, 2.0, 3.0))
-                    .mass(0.1)
                     .gravity_enabled(true)
                     .sleep_threshold(None)
                     .build();
 
                 let ball_rb_handle = bodies.insert(ball_rb);
-
-                //Ball::new(0.7)
                 let shape =
                     ShapeHandle::new(ncollide3d::shape::Cuboid::new(Vector3::new(0.5, 0.5, 0.5)));
                 let ball_col = ColliderDesc::new(shape)
@@ -183,6 +188,7 @@ impl InnerState {
                     // to the inertial properties of the body part it is attached to.
                     // Default: 0.0
                     .density(1.3)
+                    //                    .ccd_enabled(true)
                     .build(BodyPartHandle(ball_rb_handle, 0));
 
                 let ball_col_handle = colliders.insert(ball_col);
